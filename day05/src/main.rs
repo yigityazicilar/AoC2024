938842e0e7ad1b90fb761d7cebbb0f48515f159d
Orhan Yiğit Yazıcılar <yigityazicilar@gmail.com>
2024-12-06 15:49:36 +0300

    Day05 Completed

---
+++ day05/src/main.rs
@@ -0,0 +1,117 @@
+use std::{collections::{HashMap, HashSet}, fs::read_to_string};
+
+fn main() {
+    let part1_solution = part1("input.txt");
+    println!("Day 5 Part 1 Solution: {}", part1_solution);
+    
+    let part2_solution = part2("input.txt");
+    println!("Day 5 Part 2 Solution: {}", part2_solution);
+}
+
+fn part1(path: &str) -> i64 {
+    let content = read_to_string(path).unwrap().split("\n\n").map(|x| x.to_string()).collect::<Vec<String>>();
+    let mut rules_map: HashMap<i64, HashSet<i64>> = HashMap::new();
+    content[0].lines().for_each(|line| {
+        let rule = line.split("|").map(|x| x.parse::<i64>().unwrap()).collect::<Vec<i64>>();
+        if rules_map.contains_key(&rule[0]) {
+            let after_set = rules_map.get_mut(&rule[0]).unwrap();
+            after_set.insert(rule[1]);
+        } else {
+            let mut after_set = HashSet::new();
+            after_set.insert(rule[1]);
+            rules_map.insert(rule[0], after_set);
+        }
+    });
+
+    let books = content[1].lines().map(|pages| {
+        let page = pages.split(",").map(|x| x.parse::<i64>().unwrap()).collect::<Vec<i64>>();
+        page
+    }).collect::<Vec<Vec<i64>>>();
+    
+    let mid_values = books.iter().map(|pages| {
+        let mut seen_pages: HashSet<i64> = HashSet::new();
+        for page in pages {
+            let after_pages_option = rules_map.get(page);
+            if let Some(after_pages) = after_pages_option {
+                if seen_pages.difference(after_pages).count() != seen_pages.len() {
+                    return 0;
+                }
+            }
+            seen_pages.insert(*page);
+        }
+        
+        pages[pages.len().div_euclid(2)]
+    }).collect::<Vec<i64>>();
+
+    mid_values.into_iter().sum()
+}
+
+fn part2(path: &str) -> i64 {
+    let content = read_to_string(path).unwrap().split("\n\n").map(|x| x.to_string()).collect::<Vec<String>>();
+    let mut rules_map: HashMap<i64, HashSet<i64>> = HashMap::new();
+    content[0].lines().for_each(|line| {
+        let rule = line.split("|").map(|x| x.parse::<i64>().unwrap()).collect::<Vec<i64>>();
+        if rules_map.contains_key(&rule[0]) {
+            let after_set = rules_map.get_mut(&rule[0]).unwrap();
+            after_set.insert(rule[1]);
+        } else {
+            let mut after_set = HashSet::new();
+            after_set.insert(rule[1]);
+            rules_map.insert(rule[0], after_set);
+        }
+    });
+
+    let books = content[1].lines().map(|pages| {
+        let page = pages.split(",").map(|x| x.parse::<i64>().unwrap()).collect::<Vec<i64>>();
+        page
+    }).collect::<Vec<Vec<i64>>>();
+
+    let wrong_books: Vec<Vec<i64>> = books.into_iter().filter(|pages| {
+        let mut seen_pages: HashSet<i64> = HashSet::new();
+        for page in pages {
+            let after_pages_option = rules_map.get(page);
+            if let Some(after_pages) = after_pages_option {
+                if seen_pages.difference(after_pages).count() != seen_pages.len() {
+                    return true;
+                }
+            }
+            seen_pages.insert(*page);
+        }
+        false
+    }).collect();
+    
+    wrong_books.into_iter().map(|pages| {
+        let mut correct_book = Vec::new();
+        let pages_len = pages.len();
+        let mut remaining_pages: HashSet<i64> = HashSet::from_iter(pages);
+        for _ in 0..pages_len {
+            let mut candidates: HashSet<i64> = HashSet::from_iter(remaining_pages.clone());
+            for page in &remaining_pages {
+                let after_pages_option = rules_map.get(page);
+                if let Some(after_pages) = after_pages_option {
+                    candidates = candidates.difference(after_pages).map(|x| *x).collect();
+                }
+            }
+            let next_page = candidates.into_iter().next().unwrap();
+            remaining_pages.remove(&next_page);
+            correct_book.push(next_page);
+        }
+        correct_book[correct_book.len().div_euclid(2)]
+    }).sum()
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part1() {
+        assert_eq!(part1("example.txt"), 143);
+    }
+
+    #[test]
+    fn test_part2() {
+        assert_eq!(part2("example.txt"), 123);
+    }
+}
