c1431c7af736f8440af3234cda7aa619e4d14a32
Orhan Yiğit Yazıcılar <yigityazicilar@gmail.com>
2024-12-07 02:39:18 +0300

    Prettified Day 6 and improved efficiency.

--- day06/src/main.rs
+++ day06/src/main.rs
@@ -1,8 +1,8 @@
-use std::collections::HashSet;
+use std::collections::{BTreeSet, HashSet};
 
 use advent_of_code_utils::read_file;
 
-#[derive(Debug, PartialEq, Eq, Hash, Clone, Copy)]
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Hash, Clone, Copy)]
 enum Direction {
     Up,
     Down,
@@ -10,185 +10,210 @@ enum Direction {
     Right,
 } 
 
+impl Direction {
+    fn right_turn(&self) -> Self {
+        match self {
+            Self::Up => Self::Right,
+            Self::Down => Self::Left,
+            Self::Left => Self::Up,
+            Self::Right => Self::Down,
+        }
+    }
+    
+    fn left_turn(&self) -> Self {
+        match self {
+            Self::Up => Self::Left,
+            Self::Down => Self::Right,
+            Self::Left => Self::Down,
+            Self::Right => Self::Up,
+        }
+    }
+}
+
+#[derive(Debug)]
+struct Input {
+    row_obstacles: Vec<BTreeSet<usize>>,
+    col_obstacles: Vec<BTreeSet<usize>>,
+    start_pos: Position,
+    num_rows: usize,
+    num_cols: usize,
+}
+
+type Position = (usize, usize, Direction);
+
+impl Input {
+    fn new(path: &str) -> Self {
+        let grid: Vec<Vec<char>> = read_file(path)
+            .iter()
+            .map(|line| line.chars().collect())
+            .collect();
+        
+        let start_pos: Position = grid.iter().enumerate().find_map(|(r, row)| {
+            row.iter().enumerate().find_map(|(c, &cell)| {
+                match cell {
+                    '^' => Some((r, c, Direction::Up)),
+                    'v' => Some((r, c, Direction::Down)),
+                    '<' => Some((r, c, Direction::Left)),
+                    '>' => Some((r, c, Direction::Right)),
+                    _ => None,
+                }
+            })
+        }).unwrap(); 
+
+        let num_rows = grid.len();
+        let num_cols = grid[0].len();
+        
+        let mut row_obstacles: Vec<BTreeSet<usize>> = vec![BTreeSet::new(); num_rows];
+        let mut col_obstacles: Vec<BTreeSet<usize>> = vec![BTreeSet::new(); num_cols];
+        for r in 0..num_rows {
+            for c in 0..num_cols {
+                if grid[r][c] == '#' {
+                    row_obstacles[r].insert(c);
+                    col_obstacles[c].insert(r);
+                }
+            }
+        }
+
+        Self { row_obstacles, col_obstacles, start_pos, num_rows, num_cols }
+    }
+    
+    fn insert_obstacle(&mut self, r: usize, c: usize) {
+        self.row_obstacles[r].insert(c);
+        self.col_obstacles[c].insert(r);
+    }
+
+    fn remove_obstacle(&mut self, r: usize, c: usize) {
+        self.row_obstacles[r].remove(&c);
+        self.col_obstacles[c].remove(&r);
+    }
+}
+
 fn main() {
     let part1_solution = part1("input.txt");
-    println!("Day 6 Part 1 Solution: {}", part1_solution.len());
+    println!("Day 6 Part 1 Solution: {}", part1_solution);
     
     let part2_solution = part2("input.txt");
     println!("Day 6 Part 2 Solution: {}", part2_solution);
 }
 
-fn part1(path: &str) -> HashSet<(usize, usize)> {
-    let grid: Vec<Vec<char>> = read_file(path)
-        .iter()
-        .map(|line| line.chars().collect())
-        .collect();
+fn part1(path: &str) -> usize {
+    let input= Input::new(path);
 
-    let start_pos: (usize, usize, Direction) = grid.iter().enumerate().find_map(|(r, row)| {
-        row.iter().enumerate().find_map(|(c, &cell)| {
-            match cell {
-                '^' => Some((r, c, Direction::Up)),
-                'v' => Some((r, c, Direction::Down)),
-                '<' => Some((r, c, Direction::Left)),
-                '>' => Some((r, c, Direction::Right)),
-                _ => None,
-            }
-        })
-    }).unwrap();
+    let direction_changes = find_path(&input).expect("The default path is an infinite loop.");
+    let path = get_path(input.start_pos, direction_changes, input.num_rows, input.num_cols); 
     
-    let mut path: Vec<(usize, usize, Direction)> = vec![start_pos];
-    loop {
-        let (r,c, dir) = path.last().unwrap();
-        match dir {
-            Direction::Up => {
-                if *r == 0 {
-                    break;
-                }
-                if grid[r-1][*c] == '#' {
-                    path.push((*r, *c, Direction::Right))
-                } else {
-                    path.push((r-1, *c, Direction::Up))
-                }
-            }
-            Direction::Down => {
-                if *r == grid.len() - 1 {
-                    break;
-                }
-                if grid[r+1][*c] == '#' {
-                    path.push((*r, *c, Direction::Left))
-                } else {
-                    path.push((r+1, *c, Direction::Down))
-                }
-            }
-            Direction::Left => {
-                if *c == 0 {
-                    break;
-                }
-                if grid[*r][c-1] == '#' {
-                    path.push((*r, *c, Direction::Up))
-                } else {
-                    path.push((*r, c-1, Direction::Left))
-                }
-            }
-            Direction::Right => {
-                if *c == grid[0].len() - 1 {
-                    break;
-                }
-                if grid[*r][c+1] == '#' {
-                    path.push((*r, *c, Direction::Down))
-                } else {
-                    path.push((*r, c+1, Direction::Right))
-                }
-            }
-        }
-    }
-    let distinct_positions = path.into_iter().map(|(r, c, _)| (r, c)).collect::<HashSet<(usize, usize)>>();
-    distinct_positions
+    path.len()
 }
 
-fn part2(path: &str) -> i64 {
-    let mut grid: Vec<Vec<char>> = read_file(path)
-        .iter()
-        .map(|line| line.chars().collect())
-        .collect();
+fn part2 (path: &str) -> usize {
+    let mut input= Input::new(path);
 
-    let start_pos: (usize, usize, Direction) = grid.iter().enumerate().find_map(|(r, row)| {
-        row.iter().enumerate().find_map(|(c, &cell)| {
-            match cell {
-                '^' => Some((r, c, Direction::Up)),
-                'v' => Some((r, c, Direction::Down)),
-                '<' => Some((r, c, Direction::Left)),
-                '>' => Some((r, c, Direction::Right)),
-                _ => None,
+    let direction_changes = find_path(&input).expect("The default path is an infinite loop.");
+    let mut potential_obstacles = get_path(input.start_pos, direction_changes, input.num_rows, input.num_cols);
+    potential_obstacles.retain(|(r, c)| *r != input.start_pos.0 || *c != input.start_pos.1); 
+
+    let mut time_paradox_count = 0;
+    for (r, c) in potential_obstacles {
+        input.insert_obstacle(r, c);
+        if let Err(_) = find_path(&input) {
+            time_paradox_count += 1;
+        }
+        input.remove_obstacle(r, c);
+    }
+
+    time_paradox_count
+}
+
+
+fn find_path(input: &Input) -> Result<Vec<Position>, ()> {
+    let Input { row_obstacles, col_obstacles, start_pos, .. } = input;
+    let mut curr_pos: Position = *start_pos;
+    let mut obstacles_hit: BTreeSet<Position> = BTreeSet::new();
+    let mut direction_changes: Vec<Position> = vec![];
+    while let Some((r, c, dir)) = next_obstacle(curr_pos, &row_obstacles, &col_obstacles) {
+        if !obstacles_hit.insert((r, c, dir)) {
+            return Err(());
+        }
+        match dir {
+            Direction::Up => {
+                curr_pos = (r + 1, c, dir.right_turn());
             }
-        })
-    }).unwrap();
-
-    let mut initial_path = part1(path);
-    initial_path.remove(&(start_pos.0, start_pos.1));
-    
-    let mut time_paradox_count: i64 = 0;
-    for (obs_r, obs_c) in initial_path {
-        grid[obs_r][obs_c] = '#';
-        let mut seen_pos: HashSet<(usize, usize, Direction)> = HashSet::from_iter(vec![start_pos]);
-        let mut path: Vec<(usize, usize, Direction)> = vec![start_pos];
-        loop {
-            let (r,c, dir) = path.last().unwrap();
-            match dir {
-                Direction::Up => {
-                    if *r == 0 {
-                        break;
-                    }
-                    let new_pos;
-                    if grid[r-1][*c] == '#' {
-                        new_pos = (*r, *c, Direction::Right);
-                    } else {
-                        new_pos = (r-1, *c, Direction::Up);
-                    }
-                    if seen_pos.contains(&new_pos) {
-                        time_paradox_count += 1;
-                        break;
-                    }
-                    seen_pos.insert(new_pos);
-                    path.push(new_pos);
-                }
-                Direction::Down => {
-                    if *r == grid.len() - 1 {
-                        break;
-                    }
-                    let new_pos;
-                    if grid[r+1][*c] == '#' {
-                        new_pos = (*r, *c, Direction::Left);
-                    } else {
-                        new_pos = (r+1, *c, Direction::Down);
-                    }
-                    if seen_pos.contains(&new_pos) {
-                        time_paradox_count += 1;
-                        break;
-                    }
-                    seen_pos.insert(new_pos);
-                    path.push(new_pos);
-                }
-                Direction::Left => {
-                    if *c == 0 {
-                        break;
-                    }
-                    let new_pos;
-                    if grid[*r][c-1] == '#' {
-                        new_pos = (*r, *c, Direction::Up);
-                    } else {
-                        new_pos = (*r, c-1, Direction::Left);
-                    }
-                    if seen_pos.contains(&new_pos) {
-                        time_paradox_count += 1;
-                        break;
-                    }
-                    seen_pos.insert(new_pos);
-                    path.push(new_pos);
-                }
-                Direction::Right => {
-                    if *c == grid[0].len() - 1 {
-                        break;
-                    }
-                    let new_pos;
-                    if grid[*r][c+1] == '#' {
-                        new_pos = (*r, *c, Direction::Down);
-                    } else {
-                        new_pos = (*r, c+1, Direction::Right);
-                    }
-                    if seen_pos.contains(&new_pos) {
-                        time_paradox_count += 1;
-                        break;
-                    }
-                    seen_pos.insert(new_pos);
-                    path.push(new_pos);
-                }
+            Direction::Down => {
+                curr_pos = (r - 1, c, dir.right_turn());
+            }
+            Direction::Left => {
+                curr_pos = (r, c + 1, dir.right_turn());
+            }
+            Direction::Right => {
+                curr_pos = (r, c - 1, dir.right_turn());
             }
         }
-        
-        grid[obs_r][obs_c] = '.';
+        direction_changes.push(curr_pos);
     }
-    time_paradox_count
+    
+    Ok(direction_changes)
+}
+
+fn next_obstacle(curr_pos: Position, row_obstacles: &Vec<BTreeSet<usize>>, col_obstacles: &Vec<BTreeSet<usize>>) -> Option<Position> {
+    let (r, c, dir) = curr_pos;
+    match dir {
+        Direction::Up => {
+            col_obstacles[c].iter().rev().find(|&&row| row < r).map_or(None, |&row| Some((row, c, dir)))
+        }
+        Direction::Down => {
+            col_obstacles[c].iter().find(|&&row| row > r).map_or(None, |&row| Some((row, c, dir)))
+        }
+        Direction::Left => {
+            row_obstacles[r].iter().rev().find(|&&col| col < c).map_or(None, |&col| Some((r, col, dir)))
+        }
+        Direction::Right => {
+            row_obstacles[r].iter().find(|&&col| col > c).map_or(None, |&col| Some((r, col, dir)))
+        }
+    }
+}
+
+fn get_path(start_pos: Position, direction_changes: Vec<Position>, num_rows: usize, num_cols: usize) -> HashSet<(usize, usize)> {
+    let mut path = HashSet::new();
+    let (mut start_row, mut start_col, mut start_dir) = start_pos;
+    for (next_row, next_col, next_dir) in direction_changes {
+        if start_row == next_row {
+            for col in start_col.min(next_col)..=start_col.max(next_col) {
+                path.insert((start_row, col));
+            }
+            start_col = next_col;
+        } else if next_col == start_col {
+            for row in start_row.min(next_row)..=start_row.max(next_row) {
+                path.insert((row, start_col));
+            }
+            start_row = next_row;
+        }
+        start_dir = next_dir;
+    }
+
+    match start_dir {
+        Direction::Up => {
+            for row in 0..start_row {
+                path.insert((row, start_col));
+            }
+        }
+        Direction::Down => {
+            for row in start_row..num_rows {
+                path.insert((row, start_col));
+            }
+        }
+        Direction::Left => {
+            for col in 0..start_col {
+                path.insert((start_row, col));
+            }
+        }
+        Direction::Right => {
+            for col in start_col..num_cols {
+                path.insert((start_row, col));
+            }
+        }
+    }
+
+    path
 }
 
 #[cfg(test)]
@@ -197,7 +222,7 @@ mod tests {
 
     #[test]
     fn test_part1() {
-        assert_eq!(part1("example.txt").len(), 41);
+        assert_eq!(part1("example.txt"), 41);
     }
 
     #[test]
